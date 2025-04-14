c83cc49a4afbd8743912184f09d8e5625623168f
Timothy Lim <timlimytn@gmail.com>
2025-04-14 16:35:20 +0800

    Updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nk6di196if8"
+    "revision": "0.ippacaka1do"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
